d9e1ee5f3f7b6c30ba74f9ec5a39a41ead76cde4
groszgabor <ittisvagyok@gmail.com>
2023-08-02 10:44:55 +0200

    Adds query param city.name to GetCollection

--- src/Controller/LivisApiMiddlewareStatisticsController.php
+++ src/Controller/LivisApiMiddlewareStatisticsController.php
@@ -41,14 +41,14 @@ class LivisApiMiddlewareStatisticsController extends ControllerBase {
   protected $client;
 
   /**
-   * Temporary store factory
+   * Temporary store factory.
    *
    * @var Drupal\Core\TempStore\SharedTempStoreFactory
    */
   protected $tempStoreFactory;
 
   /**
-   * Shared temporary store
+   * Shared temporary store.
    *
    * @var Drupal\Core\TempStore\SharedTempStore
    */
@@ -74,8 +74,7 @@ class LivisApiMiddlewareStatisticsController extends ControllerBase {
   public function __construct(
     LivisApiMiddlewareAuthenticationManager $auth_manager,
     Settings $settings,
-    SharedTempStoreFactory $temp_store_factory)
-    {
+    SharedTempStoreFactory $temp_store_factory) {
     $this->settings = $settings;
     $this->authManager = $auth_manager;
     $this->client = new Client([
@@ -100,11 +99,13 @@ class LivisApiMiddlewareStatisticsController extends ControllerBase {
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   Original request.
+   * @param int $id
+   *   ID of the statistics resource.
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    *   The response.
    */
-  public function handleRequest(Request $request, int $id): JsonResponse {
+  public function handleGetRequest(Request $request, int $id): JsonResponse {
     $response = $this->authManager->getToken(!$this->secondAttemptLeft);
 
     if (!isset($response['token'])) {
@@ -122,9 +123,48 @@ class LivisApiMiddlewareStatisticsController extends ControllerBase {
       $this->secondAttemptLeft = FALSE;
       $this->tempStore->set('expired', TRUE);
       $this->tempStore->delete('token');
-      $response = $this->handleRequest($request, $id);
+      $response = $this->handleGetRequest($request, $id);
     }
-    else if ($status_code == 200 || $status_code == 201) {
+    elseif ($status_code == 200 || $status_code == 201) {
+      $response = new JsonResponse(json_decode($response->getBody()), $status_code);
+    }
+    else {
+      $response = new JsonResponse(json_decode($response->getBody()->getContents()), $status_code);
+    }
+
+    return $response;
+  }
+
+  /**
+   * Handle incoming request for inventory endpoint.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   Original request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   The response.
+   */
+  public function handleGetCollectionRequest(Request $request): JsonResponse {
+    $response = $this->authManager->getToken(!$this->secondAttemptLeft);
+
+    if (!isset($response['token'])) {
+      return new JsonResponse($response, $response['status_code']);
+    }
+    else {
+      $this->token = $response['token'];
+    }
+
+    $response = $this->sendApiGetCollectionRequest($request);
+
+    $status_code = $response->getStatusCode();
+
+    if ($status_code == 401 && $this->secondAttemptLeft) {
+      $this->secondAttemptLeft = FALSE;
+      $this->tempStore->set('expired', TRUE);
+      $this->tempStore->delete('token');
+      $response = $this->handleGetCollectionRequest($request);
+    }
+    elseif ($status_code == 200 || $status_code == 201) {
       $response = new JsonResponse(json_decode($response->getBody()), $status_code);
     }
     else {
@@ -139,6 +179,8 @@ class LivisApiMiddlewareStatisticsController extends ControllerBase {
    *
    * @param Symfony\Component\HttpFoundation\Request $request
    *   The original Symfony request.
+   * @param int $id
+   *   ID of the statistics resource.
    *
    * @return GuzzleHttp\Psr7\Response
    *   The API response.
@@ -152,6 +194,44 @@ class LivisApiMiddlewareStatisticsController extends ControllerBase {
     ];
     $path = $this->settings->get('livis_api')['statistics']['path'];
     $path = $path . '/' . $id;
+    try {
+      $response = $this->client->request('GET', $path, $options);
+    }
+    catch (ClientException | ServerException $e) {
+
+      return $e->getResponse();
+    }
+
+    return $response;
+  }
+
+  /**
+   * Creates and sends the request to HOME API Inventory endpoint.
+   *
+   * @param Symfony\Component\HttpFoundation\Request $request
+   *   The original Symfony request.
+   *
+   * @return GuzzleHttp\Psr7\Response
+   *   The API response.
+   */
+  protected function sendApiGetCollectionRequest(Request $request): Response {
+
+    $query = $request->query->all();
+
+    // PHP turns "." characters in $_GET and $_POST parameters into "_".
+    if (array_key_exists('city_name', $query)) {
+      $query['city.name'] = $query['city_name'];
+      unset($query['city_name']);
+    }
+
+    $options = [
+      'headers' => [
+        'Authorization' => 'Bearer ' . $this->token,
+      ],
+      'query' => $query,
+    ];
+    $path = $this->settings->get('livis_api')['statistics']['path'];
+
     try {
       $response = $this->client->request('GET', $path, $options);
     }
